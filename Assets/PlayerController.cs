8649e06d757bb8c2c5e6ed0f83f686d7def11be1
カウピーTWICE大好き <gensikaku@gmail.com>
2020-03-27 22:43:00 +0900

    15

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rgbody;
+    public float walkSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rgbody.AddForce(0,0,z*walkSpeed);
+    }
+}
